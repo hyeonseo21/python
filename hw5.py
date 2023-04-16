2a70e76f5469ddd169dad19fa6fe496269090323
hyeonseo21 <126944240+hyeonseo21@users.noreply.github.com>
2023-04-16 18:55:44 +0900

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,12 +1,19 @@
 def multiplication(r):
-    for i in range(1,10):
-        print(f"{r}X{i}={i*r:3d}")
+    for i in range(2,6):
+        print(f"{i}X{r}={i*r:3d}{' '*5}",end='')
+    print(' ',end='\n')
+
+
+def multiplication2(s):
+    for t in range(6,10):
+        print(f"{t}X{s}={t*s:3d}{' '*5}",end='')
+    print(' ',end='\n')
     
         
-for r in range(2,10):
+for r in range(1,10):
     multiplication(r)
-    
 
+print(' ',end='\n')
 
-
- 
+for s in range(1,10):
+    multiplication2(s)
